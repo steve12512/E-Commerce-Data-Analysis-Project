3132d3104975f6e5179ca8643d2bbd2ca5260b82
steve12512 <86954812+steve12512@users.noreply.github.com>
2024-07-18 23:04:25 +0300

    read files

--- cleo.py
+++ cleo.py
@@ -1,1 +1,51 @@
-import pandas
+import pandas as pd
+
+def read_files():
+    #this function reads our datasets and saves them as dataframe objects
+    dataframe1 = pd.read_excel('reviews data.xlsx')
+    dataframe2 = pd.read_excel('Booking Stats.xlsx')
+
+    return dataframe1, dataframe2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#MAIN
+dataframe1, dataframe2 = read_files()
+print(dataframe1.tail())
