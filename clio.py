90a09d0199f976d79ede5098914da059c1ac8352
steve12512 <86954812+steve12512@users.noreply.github.com>
2024-07-23 23:16:57 +0300

    edited reading of df1

--- clio.py
+++ clio.py
@@ -7,7 +7,7 @@ def read_files():
     return dataframe1, dataframe2
 
 def read_df1():
-    sheets_dict = pd.read_excel('Booking Stats.xlsx', sheet_name=None)
+    sheets_dict = pd.read_excel('unimportant/Booking Stats.xlsx', sheet_name=None)
     codes_prices_df = sheets_dict['Codes & Prices']
 
     # Initialize an empty list to store DataFrames
@@ -29,7 +29,7 @@ def read_df1():
     combined_df = pd.concat(df_list, ignore_index=True)
     combined_df = merge_with_prices(combined_df)
 
-
+    #combined_df.drop_duplicates()
     return combined_df
 
 
@@ -58,34 +58,32 @@ def merge_with_prices(df):
     unpivoted_df['month'] = unpivoted_df['month'].map(reverse_month_mapping)
 
 
-    #now finally add the price of the product to dataframe1
-    # Rename columns in unpivoted_df to match df
+    #rename columns in unpivoted_df to match df
     unpivoted_df.rename(columns={'Product Code': 'product_code'}, inplace=True)
 
+    #remove language shortcuts and '_'s in both dataframes, so that their product codes match
     unpivoted_df['product_code'] = unpivoted_df['product_code'].apply(strip_language_code)
+    #df['product_code'] = df['product_code'].str.replace(r"_.*|([A-Z]{2})$", "", regex=True)
 
 
 
 
-
-    # Merge the dataframes
+    #now finally add the price of the product to dataframe1
     merged_df = pd.merge(df, unpivoted_df, on=['product_code', 'month'], how='left')
 
     # Rename the 'Price' column to 'Ticket Price'
     merged_df.rename(columns={'Price': 'Ticket Price'}, inplace=True)
-    print('aaaa')
 
 
-
-    price_df.to_excel('price.xlsx', index= False)
-    unpivoted_df.to_excel('unpivoted.xlsx', index= False)
+    price_df.to_excel('unimportant/price.xlsx', index= False)
+    unpivoted_df.to_excel('unimportant/unpivoted.xlsx', index= False)
 
     return merged_df
 
 
 
 def strip_language_code(code):
-    #
+    #modify the dataset so as to remove '_', and Language shortcuts, in order to properly map product codes to corresponding ticket prices from different excel sheets saved as different dataframes.
     
     languages = ['GR', 'EN', 'FR', 'DE', 'IT', 'ES']
 
@@ -94,7 +92,7 @@ def strip_language_code(code):
 
 
     if code[-2:] in languages:
-        return code[:-3]
+        return code[:-2]
     return code
 
 
@@ -104,7 +102,7 @@ def strip_language_code(code):
 
 def read_df2():
 
-    dataframe2 = pd.read_excel('reviews data.xlsx')
+    dataframe2 = pd.read_excel('unimportant/reviews data.xlsx')
     return dataframe2
 
 
