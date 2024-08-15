f185dd5d7ead70eff455d40956b4238ca617e240
steve12512 <stevekalelis@hotmail.com>
2024-08-15 23:28:47 +0300

    trying to add jupyter file

--- clio.py
+++ clio.py
@@ -3,10 +3,11 @@ import pandas as pd
 from functions import read_files, save_to_excel, successful_tour_looks_like, which_tours_go_together, edit_dfs, which_tours_do_we_recommend_to_a_traveller, add_df2_profit
 
 
-
+"""
 #this is our main function file
 #From here and on, our script starts executing
 dataframe1, dataframe2 = read_files()
+
 #how does a successful tour look like?
 successful_tour_looks_like(dataframe1, dataframe2)
 
@@ -16,8 +17,6 @@ go_together = which_tours_go_together(dataframe1, dataframe2)
 
 #do a minor edit in df1 so as to have all split product codes saved as a set
 dataframe1, dataframe2 = edit_dfs(dataframe1, dataframe2)
-print(dataframe1['split_product_codes'].head(20))
-print(dataframe2['split_product_codes'].head(20))
 
 #add profit to dataframe2
 dataframe2 = add_df2_profit(dataframe1, dataframe2)
@@ -28,3 +27,5 @@ which_tours_do_we_recommend_to_a_traveller(dataframe1, dataframe2, go_together)
 
 
 save_to_excel(dataframe1, dataframe2)
+
+"""
