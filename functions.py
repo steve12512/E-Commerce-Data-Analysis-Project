ad17b5c879662a175446c1f768854267d20425b6
steve12512 <86954812+steve12512@users.noreply.github.com>
2024-08-17 22:32:03 +0300

    added lots more

--- functions.py
+++ functions.py
@@ -385,11 +385,43 @@ def codes_to_profit(key, df1):
 
 def which_tours_do_we_recommend_to_a_traveller(dataframe1, dataframe2, go_together):
     #which tours do we recommend to a traveller? first make a copy of our dataframes to operate upon
-
     df1= dataframe1.copy()
     df2 = dataframe2.copy()
     together = go_together.copy()
 
     #filter the listings in df2 that have had a rating higher than 4
     liked_tours = df2[df2['Experience'].isin(['Excellent (5 stars)', 'Positive (4 stars)', 'Positive \n(4 stars)' , 'Excellent (5*)', 'Positive (4*)', '5*', '4*'])]
-    liked_tours.to_excel('unimportant/liked.xlsx', index =False)
+
+    #now do a pivot operation. keep the name of our product as index, and pivot the experience column into 2 columns, the 4 and 5 ratings, and count the occurence of each review for the afore mentioned product
+    pivot_df = liked_tours.pivot_table(index = 'Product Code and Name', columns = 'Experience', aggfunc = 'size', fill_value = 0).reset_index()
+  
+    #calculate the 'successful_experience' as the sum of the experience ratings columns
+    pivot_df['successful_experience'] = pivot_df['Excellent (5 stars)'] + pivot_df['Excellent (5*)'] + pivot_df['Positive \n(4 stars)'] + pivot_df['Positive (4 stars)'] + pivot_df['Positive (4*)']
+
+    #aggregate additional information (Profit, Country, Month)
+    additional_info = liked_tours.groupby(['Product Code and Name', 'Country', 'month']).agg(
+        total_profit=('Profit', 'sum'),
+        count_listings=('Product Code and Name', 'size')
+    ).reset_index()
+    
+    
+    #merge the pivot table with additional information
+    result1 = pd.merge(pivot_df, additional_info, on='Product Code and Name', how='left')
+
+    #sort the result by 'total_profit' in descending order
+    result1 = result1.sort_values(by='total_profit', ascending=False)
+
+    #now we ll do the same but we ll be using df1
+    result2 = df1.groupby(['Country', 'month', 'product_title']).agg(
+        product_title_occurences=('product_title', 'count'),
+        total_profit=('Profit', 'sum'),
+        total_travellers=('num_of_travellers', 'sum')
+    ).reset_index()
+
+    result2 = result2.sort_values(by=['Country', 'total_profit'], ascending=[True, False])
+
+    # Keep only the top 5 listings per country based on 'total_profit'
+    top_5_per_country = result2.groupby('Country').head(5)
+    
+    result1.to_excel('questions/recommended1.xlsx', index =False)
+    result2.to_excel('questions/recommended2.xlsx', index =False)
